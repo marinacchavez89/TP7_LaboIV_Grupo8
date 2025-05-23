3af3a72560b9c4e3b4641a8bd455d18a1ce114ab
milidub <milagrosdubuis@hotmail.com>
2025-05-23 18:08:56 -0300

    ordene algunas carpetas en capas + conexion a db + lib .jar

---
+++ src/dao/Conexion.java
@@ -0,0 +1,55 @@
+package daoImpl;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import javax.swing.JOptionPane;
+
+public class Conexion {
+	public static Conexion instancia;
+	private Connection connection;
+	
+	private Conexion()
+	{
+		try
+		{
+			Class.forName("com.mysql.jdbc.Driver");
+			//pass: root / 1234 (mari)
+			this.connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/SegurosGroup","root","root");
+			this.connection.setAutoCommit(false);
+		}
+		catch(Exception e)
+		{	
+			JOptionPane.showMessageDialog(null, "Error al conectar a la base de datos.\nVerifique usuario, contraseña o estado del servidor.", "Error de conexión", JOptionPane.ERROR_MESSAGE);
+		    e.printStackTrace();
+		    System.exit(1); //  Cierra todo el programa
+		}
+	}
+	public static Conexion getConexion()   
+	{								
+		if(instancia == null)
+		{
+			instancia = new Conexion();
+		}
+		return instancia;
+	}
+
+	public Connection getSQLConexion() 
+	{
+		return this.connection;
+	}
+	
+	public void cerrarConexion()
+	{
+		try 
+		{
+			this.connection.close();
+		}
+		catch (SQLException e) 
+		{
+			e.printStackTrace();
+		}
+		instancia = null;
+	}
+}
